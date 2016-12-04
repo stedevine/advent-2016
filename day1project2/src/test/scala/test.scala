c2c6acaa196e8a7dea30ac9358e153f4518c9c4f
Stephen Devine <ste@stedevine.com>
2016-12-04 11:00:27 -0800

    clean up

--- day1project2/src/test/scala/test.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.FunSuite
-import advent.Main
-
-class Test extends FunSuite {
-  test("sample input 1") {
-    assert(Main.getDistanceToFirstLocationVisitedTwice("R8, R4, R4, R8") == 4)
-  }
-
-}
