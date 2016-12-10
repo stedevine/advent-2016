75a38d22a0615cc93e1235b1b377766f7110fe37
Stephen Devine <ste@stedevine.com>
2016-12-09 19:02:45 -0800

    TwoFactorAuth

--- TwoFactorAuthentication/src/main/scala/main.scala
+++ TwoFactorAuthentication/src/main/scala/main.scala
@@ -78,60 +78,6 @@ object Main {
     pixelCount
 
   }
-  /*
-  def getAnswer(input: String): Int = {
-    val command = input.split(' ')
-    if (command.size == 2) {
-      // rect command
-      val x = command(1).split("x")(0).toInt
-      val y = command(1).split("x")(1).toInt
-      for (i <- Range(0, y)) {
-        for (j <- Range(0, x)) {
-          screen(i)(j) = true
-        }
-      }
-    } else {
-
-      val shift = command(4).toInt
-
-      if (command(1) == "row") {
-        // rotate the row
-        val row = command(2).split('=')(1).toInt
-        for (i <- Range(0, shift)) {
-          screen(row) = (screen(row)(screen(row).size - 1) +: screen(row)).take(screen(row).size)
-        }
-        //println("Row : " + row)
-      } else {
-        // rotate the column
-        val colIndex = command(2).split('=')(1).toInt
-
-        // get the column 
-        var column = new Array[Boolean](screen.size)
-        for (i <- Range(0, screen.size)) {
-          column(i) = screen(i)(colIndex)
-        }
-
-        // rotate the column
-        for (i <- Range(0, shift)) {
-          column = (column(column.size - 1) +: column).take(column.size)
-        }
-
-        // update the screen
-        for (i <- Range(0, screen.size)) {
-          screen(i)(colIndex) = column(i)
-        }
-      }
-
-    }
-
-    // Count lit pixels
-    var pixelCount = 0
-    for (i <- Range(0, ySize)) {
-      pixelCount += screen(i).filter(_ == true).size
-    }
-    pixelCount
-  }
-*/
 
   // Debug functions 
   def reset(): Unit = {
