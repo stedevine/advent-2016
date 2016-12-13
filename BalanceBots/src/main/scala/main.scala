e04dea22175e72d651269c52b4b89113c0f61372
Stephen Devine <stephen.devine@bigfishgames.com>
2016-12-13 15:08:56 -0800

    BalanceBots

--- BalanceBots/src/main/scala/main.scala
+++ BalanceBots/src/main/scala/main.scala
@@ -1,40 +1,78 @@
 package advent
 object Main {
-  // Blank
+  // Balance Bots
   def main(args: Array[String]): Unit = {
-    val nodes = Map[String, Node]()
-    println(getAnswer(Input.problem))
+    getAnswer(Input.problem)
   }
 
-  def ProcessInput(input: String): Node = {
-    val node: Node = null
-    if (input.startsWith("bot")) {
+  def getAnswer(input: String): Unit = {
 
+    val nodeMap = processInput(input)
+    var outputMap = Map[String, Int]()
+    // Get the bots that contain 2 values - these are the ones that can be processed
+    var actionNodeMap = nodeMap.filter(m => m._2.canProceed)
+    // Continue until there are no more processable bots
+    while (actionNodeMap.size > 0) {
+      for (entry <- actionNodeMap) {
+        val (lowKey, lowValue, highKey, highValue) = entry._2.getValues
+        if (!lowKey.contains("output")) {
+          nodeMap(lowKey).setValue(lowValue)
+        } else {
+          outputMap += (lowKey -> lowValue)
+        }
+        if (!highKey.contains("output")) {
+          nodeMap(highKey).setValue(highValue)
+        } else {
+          outputMap += (lowKey -> lowValue)
+        }
+
+      }
+      actionNodeMap = nodeMap.filter(m => m._2.canProceed)
     }
-    node
+    // Problem 2 : output0 x output1 x output2
+    println("output0 x output1 x output2 = " + outputMap("output0") * outputMap("output1") * outputMap("output2"))
   }
 
-  def getAnswer(input: String): Int = {
-    0
+  def processInput(input: String): Map[String, Bot] = {
+    var bots = Map[String, Bot]()
+    var lines = input.split("\n")
+    // Populate the Map of bots before we attempt to initialise them
+    for (line <- lines.filter(_.startsWith("bot"))) {
+      val command = line.split(" ")
+      val key = command(0).toString + command(1).toString
+      val low = command(5).toString + command(6).toString
+      val high = command(10).toString + command(11).toString
+      bots += (key.trim -> new Bot(key, low.trim, high.trim))
+    }
+    // They all exist, initialize them
+    for (line <- lines.filter(_.startsWith("value"))) {
+      val command = line.split(" ")
+      val key = command(4).toString + command(5).toString
+      bots(key.trim).setValue(command(1).toInt)
+    }
+    bots
   }
 
-  class Node(lowKey: String, highKey: String) {
+  class Bot(key: String, lowKey: String, highKey: String) {
     var values = Array[Int]()
-
+    var isUsed = false
     def setValue(v: Int): Unit = {
       values = values :+ v
+      // Problem 1 - which bot is responsible for comparing 61 and 17?
+      if (canProceed && (values(0) == 61 || values(1) == 61) && (values(0) == 17 || values(1) == 17)) {
+        println(key + " compares 61 and 17")
+      }
     }
 
     def canProceed(): Boolean = {
-      values.size == 2
+      isUsed == false && values.size == 2
     }
 
-    def getLow(): (String, Int) = {
-      (lowKey, Math.min(values(0), values(1)))
-    }
-    def getHigh(): (String, Int) = {
-      (highKey, Math.max(values(0), values(1)))
+    def getValues(): (String, Int, String, Int) = {
+      // We only need to process each bot once - if we don't mark the bot as used the
+      // program will not terminate.
+      isUsed = true
+      (lowKey, Math.min(values(0), values(1)), highKey, Math.max(values(0), values(1)))
     }
   }
 }
-
--- BalanceBots/src/test/scala/test.scala
+++ BalanceBots/src/test/scala/test.scala
@@ -10,7 +10,11 @@ value 3 goes to bot 1
 bot 1 gives low to output 1 and high to bot 0
 bot 0 gives low to output 2 and high to output 0
 value 2 goes to bot 2"""
-    assert(Main.processInput(input) == "")
+    val nodes = Main.processInput(input)
+    assert(nodes.size == 3)
+    assert(nodes("bot0").canProceed == false)
+    assert(nodes("bot1").canProceed == false)
+    assert(nodes("bot2").canProceed == true)
   }
 
 }
