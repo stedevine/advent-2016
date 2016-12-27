9f3efaf3f77f6c6b702d7441c8ae6303d80f5a88
Stephen Devine <ste@stedevine.com>
2016-12-26 16:02:15 -0800

    one time pad - no cache

--- OneTimePad/src/main/scala/main.scala
+++ OneTimePad/src/main/scala/main.scala
@@ -4,23 +4,50 @@ object Main {
   // Blank
   def main(args: Array[String]): Unit = {
     // Print the index of the  64th key for this salt
-    println(getAnswer("abc"))
+    println(getAnswer("yjdafjpo"))
   }
 
   def getAnswer(input: String): Int = {
     // build up a map of "characters that are repeated 5 times" to a list of indices at which this happens
-    val qMap = Map[Char, List[Int]]()
+    var keys = List[Int]()
+    var qMap = scala.collection.mutable.Map[Char, List[Int]]()
     var qIndex = 0
-    while (qIndex < 10000000) {
-      val hash = getHashAtIndex(input, qIndex)
-      val qChars = getQuintetCharacters(hash)
+    var tIndex = qIndex - 1000
+    while (keys.size < 64) {
+      if (tIndex >= 0) {
+        var tripletCharacter = getTripletValue(getHashAtIndex(input, tIndex))
+        if (!tripletCharacter.isEmpty) {
+          // does the triplet value exist in the qMap?
+          if (qMap.contains(tripletCharacter.get)) {
+            //   println(tIndex + " " + tripletCharacter.get + " " + qMap(tripletCharacter.get))
+
+            val indexInMap = qMap(tripletCharacter.get).reverse.head
+            if (indexInMap - tIndex > 0) {
+              //    println("within range " + (indexInMap - tIndex))
+              keys = keys :+ tIndex
+            }
+          }
+        }
+      }
+
+      val qChars = getQuintetCharacters(getHashAtIndex(input, qIndex))
       if (!qChars.isEmpty) {
-        println(qIndex.toString + " " + qChars)
+        //println(qIndex.toString + " " + qChars)
+        for (qChar <- qChars) {
+          if (qMap.contains(qChar)) {
+            qMap(qChar) = qMap(qChar) :+ qIndex
+          } else {
+            qMap(qChar) = List(qIndex)
+          }
+        }
       }
       qIndex += 1
+      tIndex += 1
     }
-
-    0
+    //println(qMap)
+    //println(keys)
+    println(keys(63))
+    keys(63)
   }
 
   def getHashAtIndex(salt: String, index: Int): String = {
@@ -45,8 +72,8 @@ object Main {
   }
 
   // Get a list of all of the characters that are repeated 5 or more times
-  def getQuintetCharacters(input: String): List[String] = {
-    input.sliding(5).filter(a => a.toString.distinct.size == 1).map(b => b.toString.head.toString).toList.distinct
+  def getQuintetCharacters(input: String): List[Char] = {
+    input.sliding(5).filter(a => a.toString.distinct.size == 1).map(b => b.toString.head).toList.distinct
   }
 
   /*
